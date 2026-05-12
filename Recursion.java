f6d8b7b3bef75600655c0b85f54f85cd5909657c
cristiant2150-creator <cristiant.2150@gmail.com>
2026-05-12 09:19:30 -0500

    Probando

---
+++ Recursion.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Recursion { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Recursion poli = new Recursion();
+
+        System.out.print("Pon un numero para los retos de recursion: ");
+        int n = sc.nextInt();
+
+        System.out.println("[R01] El factorial de " + n + " es: " + poli.g5_r01(n));
+        
+        System.out.print("[R05] Conteo hasta " + n + ": ");
+        poli.g5_r05(n);
+        System.out.println();
+    }
+
+    // Métodos de Recursión
+    public int g5_r01(int n) {
+        if (n <= 1) return 1;
+        return n * g5_r01(n - 1);
+    }
+
+    public void g5_r05(int n) {
+        if (n > 0) g5_r05(n - 1);
+        System.out.print(n + " ");
+    }
+}
