2cccf2c89377099abf3780d0a927be955a7c702e
unknown <marcos.quimba@epn.edu.ec>
2026-05-11 21:04:50 -0500

    Estructura inicial, recalco, no funcional todavia

--- src/SerieEnum/Figonachi.java
+++ src/SerieEnum/Figonachi.java
@@ -1,5 +1,14 @@
 package SerieEnum;
 
 public class Figonachi {
-
+    public static void main(String[] args) {
+        int Num = 0;
+        int n1 = 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Num);
+            int next = Num + n1;
+            Num = n1;
+            n1 = next;
+        }
+    }
 }
